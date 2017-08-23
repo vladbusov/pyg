cdb6f582c9fd8cecae61765b6f20944779853c22
Влад <to2002to2002@gmail.com>
2017-08-23 23:13:58 +0300

    first commit

---
+++ game.py
@@ -0,0 +1,10 @@
+import direct.directbase.DirectStart
+
+#Load the first environment model
+environ = loader.loadModel("models/environment")
+environ.reparentTo(render)
+environ.setScale(0.25,0.25,0.25)
+environ.setPos(-8,42,0)
+#Run the tutorial
+
+run()
